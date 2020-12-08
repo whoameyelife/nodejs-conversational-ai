9e8cce63e4b3c4a33a4e6e5fc1e0bded242c86bb
Shravan Naik <deadpool1794@Shravans-MacBook-Pro.local>
2020-12-09 00:22:54 +0530

    Added required files

---
+++ config.js
@@ -0,0 +1,2 @@
+export const API_KEY =
+  "dict.1.1.20170610T055246Z.0f11bdc42e7b693a.eefbde961e10106a4efa7d852287caa49ecc68cf";
---
+++ dictionaryAsync.js
@@ -0,0 +1,77 @@
+//Fetching the data from the text file asynchronously
+import * as fs from "fs";
+import fetch from "node-fetch";
+import { API_KEY } from "./config.js";
+
+const getTextDetail = async (text, count) => {
+  let output = [];
+  let synonyms = [];
+  return await fetch(
+    `https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${API_KEY}&lang=en-en&text=${text}`
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      let textData, textSynonyms;
+      if (json["def"].length !== 0) {
+        textData = { ...json["def"] };
+        if (textData["0"].tr) {
+          textSynonyms = textData["0"].tr;
+        }
+
+        textSynonyms.map((textObj) => {
+          if (textObj.syn !== undefined) {
+            textObj.syn.map((synonym) => {
+              synonyms.push(synonym.text);
+            });
+          }
+        });
+
+        console.log("Text: ", text.toUpperCase());
+        console.log("Part Of Speech: ", textData["0"]["pos"]);
+        console.log("Occurences: ", count);
+        console.log("Synonyms: ", synonyms, "\n");
+        console.log("------------------------------- \n");
+      } else {
+        console.log("Text:", text.toUpperCase());
+        console.log("Part Of Speech: Not Available");
+        console.log("Occurences: ", count);
+        console.log("Synonyms: Not Available \n");
+        console.log("------------------------------- \n");
+      }
+    });
+};
+
+fs.readFile("Data.txt", "utf-8", (err, data) => {
+  if (err) {
+    throw err;
+  }
+  let lineArray = data.split("\n").filter((a) => a.length !== 0);
+  let mappedText = {};
+  lineArray.map((line, index) => {
+    let textsArray = line.split(" ");
+    textsArray.map((text, index) => {
+      let filteredText = text.toLowerCase().replace(/[^a-zA-Z]/g, "");
+      if (filteredText.length > 5) {
+        if (filteredText in mappedText) {
+          mappedText[filteredText] = mappedText[filteredText] + 1;
+        } else {
+          mappedText[filteredText] = 1;
+        }
+      }
+      return true;
+    });
+    return true;
+  });
+
+  let result = Object.assign(
+    ...Object.entries(mappedText)
+      .sort(({ 1: a }, { 1: b }) => b - a)
+      .slice(0, 10)
+      .map(([c, d]) => ({ [c]: d }))
+  );
+
+  Object.entries(result).forEach((entry) => {
+    const [key, value] = entry;
+    getTextDetail(key, value);
+  });
+});
